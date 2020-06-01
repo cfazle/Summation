caf773c9dc0d6e1dd30c57eb6bbfaf6171523bbf
Fazle <cfazle@gmail.com>
2020-06-01 10:18:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Summation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare two collections of int elements.
+            int[] array1 = { 1, 3, 5, 7 };
+            List<int> list1 = new List<int>() { 1, 3, 5, 9 };
+
+            //Use Sum extension on their elements
+            int sum1 = array1.Sum();
+            int sum2 = list1.Sum();
+
+            Console.WriteLine("Sum of array:" + sum1);
+            Console.WriteLine("Sum of List:" + sum2);
+        }
+    }
+}
